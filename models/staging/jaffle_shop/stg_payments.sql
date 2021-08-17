ca9e02165d5e53f9d7fe53415d7357661da9609d
ozgenbaris1 <bariso@armut.com>
2021-08-17 10:18:15 +0300

    Update stg_payments model

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -2,7 +2,12 @@ with payments as (
     select
         id as payment_id,
         orderid as order_id,
-        amount
+        paymentmethod as payment_method,
+        status,
+
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
     from raw.stripe.payment
 )
 
