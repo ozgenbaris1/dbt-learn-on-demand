858e2f02ddceae6da2a990df40771c73033b2199
ozgenbaris1 <bariso@armut.com>
2021-08-17 11:28:44 +0300

    Add source table configuration for payment table

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -8,7 +8,7 @@ with payments as (
         -- amount is stored in cents, convert it to dollars
         amount / 100 as amount,
         created as created_at
-    from raw.stripe.payment
+    from {{ source('stripe', 'payment') }}
 )
 
 select * from payments
